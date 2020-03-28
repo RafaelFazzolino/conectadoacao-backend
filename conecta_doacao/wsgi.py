c4dfd14d1de9618d301abc6de63f8cd0b97d07a0
Rafael Fazzolino <rafaelfazzolino@MacBook-Air-de-Rafael.local>
2020-03-27 21:16:59 -0300

    Create project

---
+++ conecta_doacao/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for conecta_doacao project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conecta_doacao.settings')
+
+application = get_wsgi_application()
